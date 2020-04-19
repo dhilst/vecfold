e18be0c2ebfa2396588d140889f0409988c5232f
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-04-19 10:15:11 -0300

    Make trait public and add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::fmt::Debug;
 
-trait VecFoldResult<'a, T, E>
+pub trait VecFoldResult<'a, T, E>
 where
     T: Debug,
     E: Error + Debug + 'a,
@@ -95,5 +95,25 @@ mod test {
             (vec![&1, &3], vec![&StaticStringError("oops")]),
             r.foldr_bisect()
         );
+    }
+
+    #[test]
+    fn test_with_parse() {
+        let valid: Vec<Result<u32, _>> = "1,2,3".split(",").map(|x| x.parse::<u32>()).collect();
+
+        let invalid: Vec<Result<u32, _>> = "1,2,a".split(",").map(|x| x.parse::<u32>()).collect();
+
+        // happy path, no errors, just the values
+        assert_eq!(vec![&1, &2, &3], valid.foldr().unwrap());
+
+        // sad path returns the error
+        assert!(invalid.foldr().is_err());
+
+        // happy path, no errors, return empty error vector
+        assert_eq!((vec![&1, &2, &3], vec![]), valid.foldr_bisect());
+
+        // sad path, populate error vector
+        let (ok, _) = invalid.foldr_bisect();
+        assert_eq!(vec![&1, &2], ok);
     }
 }
