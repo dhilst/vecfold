d7eec0772f7873eade20723660690dd0d648ae83
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-04-19 09:30:13 -0300

    Add docs

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex={};
+searchIndex["lib"] = {"doc":"","i":[[8,"VecFoldResultBisect","lib","",null,null],[10,"foldr_bisect","","",0,[[["self"]]]]],"p":[[8,"VecFoldResultBisect"]]};
+addSearchOptions(searchIndex);initSearch(searchIndex);
